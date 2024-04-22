18f82d1b5c63769c66a925e28ee4f4c9044a669c
Ashraf Abd El-Monem Mahmoud El-Fallah <elfallahashraf44@gmail.com>
2024-04-22 22:22:38 +0200

    finish add date and time in add card screen

--- app/src/main/java/com/af/dentalla/ui/doctor/addCard/AddCardFragment.kt
+++ app/src/main/java/com/af/dentalla/ui/doctor/addCard/AddCardFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import com.af.dentalla.R
+import com.af.dentalla.databinding.FragmentAddCardBinding
 
 class AddCardFragment : Fragment() {
+    private lateinit var binding: FragmentAddCardBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_card, container, false)
+    ): View {
+        binding = FragmentAddCardBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 
 }
