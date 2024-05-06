b0b4222a2009abaa82696c08a6dbe2b669774822
Ashraf Abd El-Monem Mahmoud El-Fallah <elfallahashraf44@gmail.com>
2024-05-07 02:13:26 +0300

    add calender and time picker for add card fragment

--- app/src/main/java/com/af/dentalla/ui/doctor/addCard/AddCardFragment.kt
+++ app/src/main/java/com/af/dentalla/ui/doctor/addCard/AddCardFragment.kt
@@ -1,12 +1,12 @@
 package com.af.dentalla.ui.doctor.addCard
 
-import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.af.dentalla.R
 import com.af.dentalla.databinding.FragmentAddCardBinding
@@ -15,11 +15,14 @@ import com.af.dentalla.ui.patient.homeScreen.SpecialtyAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 @AndroidEntryPoint
 class AddCardFragment : Fragment() {
     private lateinit var binding: FragmentAddCardBinding
+    private var selectedDate: Date? = null
+    private var selectedTime: Date? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,64 +33,64 @@ class AddCardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.selectDate.setOnClickListener {
-            showDatePickerDialog()
-        }
+
+        setDateFromTheCalender()
         binding.selectTime.setOnClickListener {
             showTimePickerDialog()
         }
+        binding.confirmButton.setOnClickListener {
+            createFormatForDateAndTime()
+        }
         setUpAddSpecialityRecyclerView()
     }
-    private fun showDatePickerDialog() {
-        val currentDate = Calendar.getInstance()
-        // Create a date picker dialog
-        val datePickerDialog = DatePickerDialog(
-            requireContext(),
-            { view, year, month, dayOfMonth ->
-                val selectedDateCalendar = Calendar.getInstance()
-                selectedDateCalendar.set(Calendar.YEAR, year)
-                selectedDateCalendar.set(Calendar.MONTH, month)
-                selectedDateCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-                // Format the selected date
-                val simpleDateFormat =
-                    SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
-                val formattedDate = simpleDateFormat.format(selectedDateCalendar.time)
+    private fun setDateFromTheCalender() {
+        binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance().apply {
+                set(year, month, dayOfMonth)
+            }
+            selectedDate = calendar.time
+        }
+    }
 
-                // Display the selected date in the TextView
-                // selectedDateTextView.text = formattedDate
-            },
-            currentDate.get(Calendar.YEAR),
-            currentDate.get(Calendar.MONTH),
-            currentDate.get(Calendar.DAY_OF_MONTH)
-        )
-
-        // Show the date picker dialog
-        datePickerDialog.show()
+    private fun createFormatForDateAndTime() {
+        // Check if both date and time are selected
+        if (selectedDate != null && selectedTime != null) {
+            // Format the selected date
+            val formattedDate =
+                SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(selectedDate)
+            // Format the selected time
+            val formattedTime =
+                SimpleDateFormat("HH:mm:ss.SSS", Locale.getDefault()).format(selectedTime)
+            // Combine date and time
+            val formattedDateTime = "$formattedDate" + "T" + "$formattedTime" + "Z"
+            Toast.makeText(requireContext(), formattedDateTime, Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Please select both date and time",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     private fun showTimePickerDialog() {
-        val currentTime = Calendar.getInstance()
-        val hour = currentTime.get(Calendar.HOUR_OF_DAY)
-        val minute = currentTime.get(Calendar.MINUTE)
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
 
-        val timePickerDialog = TimePickerDialog(
+        TimePickerDialog(
             requireContext(),
-            TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                val selectedTimeCalendar = Calendar.getInstance()
-                selectedTimeCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
-                selectedTimeCalendar.set(Calendar.MINUTE, minute)
-
-                val simpleDateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-                val formattedTime = simpleDateFormat.format(selectedTimeCalendar.time)
-                //selectedTimeTextView.text = formattedTime
+            { _, hourOfDay, minute ->
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                calendar.set(Calendar.MINUTE, minute)
+                selectedTime = calendar.time
             },
             hour,
             minute,
             false
-        )
+        ).show()
 
-        timePickerDialog.show()
     }
 
     private fun setUpAddSpecialityRecyclerView() {
@@ -106,5 +109,6 @@ class AddCardFragment : Fragment() {
             LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
     }
 }
+
 
 
