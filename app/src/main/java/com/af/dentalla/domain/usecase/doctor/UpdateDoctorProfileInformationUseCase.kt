ed137564b39ba003ae30dcacff05a5a2841c49a6
Ashraf Abd El-Monem Mahmoud El-Fallah <elfallahashraf44@gmail.com>
2024-05-12 18:29:19 +0300

    create doctor change password use case

--- app/src/main/java/com/af/dentalla/domain/usecase/doctor/UpdateDoctorProfileInformationUseCase.kt
+++ app/src/main/java/com/af/dentalla/domain/usecase/doctor/UpdateDoctorProfileInformationUseCase.kt
@@ -4,8 +4,8 @@ import com.af.dentalla.data.remote.requests.DoctorProfileInformation
 import com.af.dentalla.domain.repository.UserRepository
 import javax.inject.Inject
 
-    class UpdateDoctorProfileInformationUseCase @Inject constructor(private val repository: UserRepository) {
-        operator fun invoke(
-            doctorProfileInformation: DoctorProfileInformation
-        ) = repository.updateDoctorProfileInformation(doctorProfileInformation)
+class UpdateDoctorProfileInformationUseCase @Inject constructor(private val repository: UserRepository) {
+    operator fun invoke(
+        doctorProfileInformation: DoctorProfileInformation
+    ) = repository.updateDoctorProfileInformation(doctorProfileInformation)
 }
