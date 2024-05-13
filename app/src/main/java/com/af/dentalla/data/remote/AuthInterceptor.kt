bda6fc96f857c574026cdb8733cea43728f1a547
RanaHisham18 <hishamrana00@gmail.com>
2024-05-13 22:35:31 +0300

    AuthInterceptor || change in intercept function to carry the response body & work on the original request, crash is solved.

--- app/src/main/java/com/af/dentalla/data/remote/AuthInterceptor.kt
+++ app/src/main/java/com/af/dentalla/data/remote/AuthInterceptor.kt
@@ -5,51 +5,73 @@ import android.util.Log
 import com.af.dentalla.data.local.DataStorePreferencesService
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.Protocol
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(private val dataStorePreferencesService: DataStorePreferencesService) :
     Interceptor {
     //    dataStorePreferencesService: DataStorePreferencesService
+//    override fun intercept(chain: Interceptor.Chain): Response {
+//            val token: String? = runBlocking {
+//                try {
+//                    dataStorePreferencesService.getToken()
+//                } catch (e: Exception) {
+//                    Log.e("AuthInterceptor", "Failed to retrieve token: ${e.message}")
+//                    null
+//                }
+//            }
+//
+//            token?.let {
+//                val request = chain.request().newBuilder()
+//                    .addHeader("Authorization", "Bearer $it")
+//                    .build()
+//                return chain.proceed(request)
+//            }
+//
+//            // If token is null, handle the case here (e.g., redirect to login screen)
+//            Log.e("AuthInterceptor", "Token is null. Handling the case...")
+//            // For demonstration purposes, return a 401 Unauthorized response
+//            return Response.Builder()
+//                .code(401)
+//                .message("Unauthorized")
+//                .protocol(Protocol.HTTP_1_1)
+//                .build()
+//        }
+
     override fun intercept(chain: Interceptor.Chain): Response {
-//                val token = dataStorePreferencesService.getToken()
-//        Log.d("interceptorKKKKKKKK", ".........")
-//        Log.d("Token interceptor", token)
-//        val request = chain.request().newBuilder()
-//            .addHeader("Authorization", "Bearer $token")
-////            .addHeader("Content-Type", "application/json")
-//            .build()
-////        }
-//        return chain.proceed(request)
-//    }
-            val token: String? = runBlocking {
-                try {
-                    dataStorePreferencesService.getToken()
-                } catch (e: Exception) {
-                    Log.e("AuthInterceptor", "Failed to retrieve token: ${e.message}")
-                    null
-                }
+        val token: String? = runBlocking {
+            try {
+                dataStorePreferencesService.getToken()
+            } catch (e: Exception) {
+                Log.e("AuthInterceptor", "Failed to retrieve token: ${e.message}")
+                null
             }
-
-            token?.let {
-                val request = chain.request().newBuilder()
-                    .addHeader("Authorization", "Bearer $it")
-                    .build()
-                return chain.proceed(request)
-            }
-
-            // If token is null, handle the case here (e.g., redirect to login screen)
-            Log.e("AuthInterceptor", "Token is null. Handling the case...")
-            // For demonstration purposes, return a 401 Unauthorized response
-            return Response.Builder()
-                .code(401)
-                .message("Unauthorized")
-                .protocol(Protocol.HTTP_1_1)
-                .build()
         }
+
+        token?.let {
+            val request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $it")
+                .build()
+            return chain.proceed(request)
+        }
+
+        Log.e("AuthInterceptor", "Token is null. Handling the case...")
+        val responseBody = ResponseBody.create("application/json".toMediaTypeOrNull(), "{\"error\":\"Unauthorized\"}")
+        return Response.Builder()
+            .request(chain.request())
+            .code(401)
+            .protocol(Protocol.HTTP_1_1)
+            .message("Unauthorized")
+            .body(responseBody)
+            .build()
     }
+
+}
 
     private fun isSignUpRequest(request: Request): Boolean {
         val pathSegments = request.url.encodedPathSegments
