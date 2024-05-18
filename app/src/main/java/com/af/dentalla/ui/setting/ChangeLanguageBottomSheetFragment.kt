833675fb5ca53a1ef8aff1166471e44db23d0294
Ashraf Abd El-Monem Mahmoud El-Fallah <elfallahashraf44@gmail.com>
2024-05-18 21:51:21 +0300

    translate change language and setting screens to Arabic

--- app/src/main/java/com/af/dentalla/ui/setting/ChangeLanguageBottomSheetFragment.kt
+++ app/src/main/java/com/af/dentalla/ui/setting/ChangeLanguageBottomSheetFragment.kt
@@ -1,15 +1,14 @@
 package com.af.dentalla.ui.setting
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.af.dentalla.R
 import com.af.dentalla.databinding.FragmentChangeLanguageBottomSheetBinding
-import com.af.dentalla.databinding.FragmentSettingBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ChangeLanguageBottomSheetFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentChangeLanguageBottomSheetBinding
     override fun onCreateView(
