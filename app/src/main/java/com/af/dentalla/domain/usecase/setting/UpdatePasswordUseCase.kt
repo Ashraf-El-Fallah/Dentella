fe428200114860700a13f8b7243ce79b1f9f2c57
Ashraf Abd El-Monem Mahmoud El-Fallah <elfallahashraf44@gmail.com>
2024-06-26 06:02:32 +0300

    refactor code in update password use case

--- app/src/main/java/com/af/dentalla/domain/usecase/setting/UpdatePasswordUseCase.kt
+++ app/src/main/java/com/af/dentalla/domain/usecase/setting/UpdatePasswordUseCase.kt
@@ -5,22 +5,23 @@ import com.af.dentalla.R
 import com.af.dentalla.data.NetWorkResponseState
 import com.af.dentalla.data.remote.requests.UserPasswords
 import com.af.dentalla.domain.repository.UserRepository
+import com.af.dentalla.domain.usecase.authentication.validations.ValidatePasswordFieldUseCase
 import com.af.dentalla.utils.GetStringUtil.getString
-import com.af.dentalla.utils.ValidationUtils
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class UpdatePasswordUseCase @Inject constructor(
     private val repository: UserRepository,
-    private val application: Application
+    private val application: Application,
+    private val validatePasswordFieldUseCase: ValidatePasswordFieldUseCase
 ) {
     fun execute(
         oldPassword: String,
         newPassword: String
     ): Flow<NetWorkResponseState<Unit>> {
         return flow {
-            if (!ValidationUtils.isPasswordValid(newPassword) || newPassword == oldPassword || !ValidationUtils.isPasswordValid(
+            if (validatePasswordFieldUseCase(newPassword) || newPassword == oldPassword || validatePasswordFieldUseCase(
                     oldPassword
                 )
             ) {
